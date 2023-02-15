c8f7943e5d82ee8a59f00a77c04114a3b6af4042
CindyDorantes <cindym.dorantes@gmail.com>
2023-02-15 00:48:51 -0600

    Update registration form

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,5 @@ class User < ApplicationRecord
   has_many :groups, foreign_key: 'user_id', dependent: :destroy
   has_many :operations, foreign_key: 'user_id', dependent: :destroy
 
-  # validates :name, presence: true, length: { maximum: 250 }
+  validates :name, presence: true, length: { maximum: 250 }
 end
