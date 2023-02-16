291fd249b558cf6c91d3b04dc306acf99de2b163
CindyDorantes <cindym.dorantes@gmail.com>
2023-02-15 23:36:39 -0600

    Create the page to add a category

--- app/models/group.rb
+++ app/models/group.rb
@@ -2,6 +2,7 @@ class Group < ApplicationRecord
   belongs_to :user, class_name: 'User', foreign_key: 'user_id'
   has_many :group_operations, foreign_key: 'operation_id'
   has_many :operations, through: :group_operations
+  has_one_attached :icon
 
   validates :name, presence: true, length: { maximum: 250 }
   validates :icon, presence: true
--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   root "welcome#index"
+  resources :groups do
+    resources :operations
+  end
 end
