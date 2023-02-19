0f486b473408ecfe78a7d6d2df73153a1b8bccc0
CindyDorantes <cindym.dorantes@gmail.com>
2023-02-18 20:48:35 -0600

    Add validation for all user inputs

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,6 @@ class User < ApplicationRecord
   has_many :operations, foreign_key: 'user_id', dependent: :destroy
 
   validates :name, presence: true, length: { maximum: 250 }
+  validates :password, presence: true, length: { minimum: 6 }
+  validates :email, presence: true
 end
